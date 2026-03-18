410d0206fea61477063967fb56c6eaa52b96b167
Ruslan Sevostyanov <zetoster@gmail.com>
2026-03-18 12:44:16 +0300

    added odeint based simulation

--- pendulum.py
+++ pendulum.py
@@ -1,12 +1,21 @@
 import pybullet as p
 import time
 import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+import numpy as np
 
 dt = 1/240 # pybullet simulation step
 q0 = 0.5   # starting position (radian)
-physicsClient = p.connect(p.GUI) # or p.DIRECT for non-graphical version
-p.setGravity(0,0,-10)
+g = 10
+L = 0.8
+gui = False
+if gui:
+    physicsClient = p.connect(p.GUI) # or p.DIRECT for non-graphical version
+else:
+    physicsClient = p.connect(p.DIRECT)
+p.setGravity(0,0,-g)
 boxId = p.loadURDF("./simple.urdf.xml", useFixedBase=True)
+
 
 # get rid of all the default damping forces
 p.changeDynamics(boxId, 1, linearDamping=0, angularDamping=0)
@@ -30,9 +39,28 @@ while t < maxTime:
     logTheta.append(pos)
     t += dt
     logTime.append(t)
-    time.sleep(dt)
+    if gui:
+        time.sleep(dt)
 p.disconnect()
 
-plt.plot(logTime, logTheta)
+def right_part(x, t):
+    return np.array([x[1],
+                     -g/L * np.sin(x[0])])
+
+# substitute with pybullet-based integration method
+theta = odeint(func=right_part,
+               y0=[q0, 0],
+               t=logTime)
+
+logThetaInt = theta[:,0]
+
+# L2: ||logTheta - logThetaInt||
+# diff = abs(logTheta-logThetaInt)
+# L2 = 1/N * sqrt(diff[0]**2 + diff[1]**2 + ...)
+# Linf = max(diff)
+
+plt.plot(logTime, logTheta, label='pybullet')
+plt.plot(logTime, logThetaInt, label='odeint')
 plt.grid(True)
+plt.legend()
 plt.show()
